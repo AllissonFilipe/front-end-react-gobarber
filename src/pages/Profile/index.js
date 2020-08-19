b8e867615bf2468d8bdf08aa4a3bc2fc3e25610a
AllissonFilipe <alissonfilipe@hotmail.com>
2020-08-19 15:56:12 -0300

    Pagina de Perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,30 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+
+import { Container } from './styles';
 
 export default function Profile() {
-    return <div />;
+    const profile = useSelector(state => state.user.profile);
+
+    function handleSubmit(data) {
+
+    }
+
+    return <Container>
+        <Form initialData={profile} onSubmit={handleSubmit}>
+            <Input name="name" placeholder="Nome Completo"/>
+            <Input name="email" type="email" placeholder="Seu Endereço de Email"/>
+
+            <hr />
+
+            <Input type="password" name="oldPassword" placeholder="Sua Senha Atual"/>
+            <Input type="password" name="password" placeholder="Nova Senha"/>
+            <Input type="password" name="confirmPassowrd" placeholder="Confirmação de Senha"/>
+
+            <button type="submit">Atualizar Perfil</button>
+        </Form>
+
+        <button type="button">Sair do GoBarber</button>
+    </Container>;
 }
